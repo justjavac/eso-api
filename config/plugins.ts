9486ff69204626b1d04994cb2b7508f9a7b7d943
迷渡 <justjavac@gmail.com>
2022-12-06 14:33:06 +0800

    添加 slug

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,1 @@
-export default ({ env }) => ({
-  graphql: {
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: true,
-      },
-    },
-  },
-});
+export default ({ env }) => ({});
