904faa6b5224cdd12827d30fbd304b67348bd0f4
迷渡 <justjavac@gmail.com>
2023-01-18 16:28:40 +0800

    server-status

--- src/api/server-status/controllers/server-status.ts
+++ src/api/server-status/controllers/server-status.ts
@@ -2,6 +2,39 @@
  * server-status controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::server-status.server-status');
+export default factories.createCoreController(
+  "api::server-status.server-status",
+  ({ strapi }) => ({
+    async update(ctx) {
+      const { servers } = await strapi.entityService.findOne(
+        "api::server-status.server-status",
+        1,
+      );
+
+      const response = await super.update(ctx);
+
+      let message = "服务器正常运行";
+      if (isOnline(servers) && !isOnline(response.data.attributes.servers)) {
+        message = "服务器正在维护中";
+      } else if (
+        !isOnline(servers) && isOnline(response.data.attributes.servers)
+      ) {
+        message = "服务器正常运行";
+      } else {
+        return response;
+      }
+
+      return await strapi.entityService.update(
+        "api::server-status.server-status",
+        1,
+        { data: { message } },
+      );
+    },
+  }),
+);
+
+function isOnline(servers: Record<string, boolean>) {
+  return Object.values(servers).every((server) => server);
+}
