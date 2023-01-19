52f646a659e798f8c104ccabf6d9c7672aef938e
迷渡 <justjavac@gmail.com>
2023-01-19 09:46:35 +0800

    quest

---
+++ src/api/quest/controllers/quest.ts
@@ -0,0 +1,7 @@
+/**
+ * quest controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::quest.quest');
---
+++ src/api/quest/routes/quest.ts
@@ -0,0 +1,7 @@
+/**
+ * quest router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::quest.quest');
