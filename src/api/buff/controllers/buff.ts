6ddb3f168c8e66f23133c65ffe8359a33eede619
迷渡 <justjavac@gmail.com>
2023-02-01 16:39:44 +0800

    buff

---
+++ src/api/buff/controllers/buff.ts
@@ -0,0 +1,7 @@
+/**
+ * buff controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::buff.buff');
---
+++ src/api/buff/services/buff.ts
@@ -0,0 +1,7 @@
+/**
+ * buff service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::buff.buff');
